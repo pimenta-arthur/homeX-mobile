4e085990dd356acf92c5947a594457be30491c83
Arthur Pimenta <arituca@gmail.com>
2019-01-10 16:52:34 -0200

    created firebase instance

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,36 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(HomeXApp());
-
-class HomeXApp extends StatelessWidget {
-  @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home: new Home(),
-      );
-    }
+Future<void> main() async {
+  final FirebaseApp app = await FirebaseApp.configure(
+    name: 'homex-db',
+    options: Platform.isIOS
+        ? const FirebaseOptions(
+            googleAppID: '1:723851736195:android:282ded12e0a45d06',
+            gcmSenderID: '723851736195',
+            databaseURL: 'https://homex-6e369.firebaseio.com',
+          )
+        : const FirebaseOptions(
+            googleAppID: '1:723851736195:android:282ded12e0a45d06',
+            apiKey: 'AIzaSyAW0nGt3ypZGVIC3p-A0uk102KTgWNUE-g',
+            databaseURL: 'https://homex-6e369.firebaseio.com',
+          ),
+  );
+  runApp(MaterialApp(
+    title: 'HomeX App',
+    home: Home(app: app),
+  ));
 }
 
 class Home extends StatefulWidget {
+  Home({this.app});
+  final FirebaseApp app;
+
   @override
   HomeState createState() {
     return new HomeState();
@@ -22,14 +38,13 @@ class Home extends StatefulWidget {
 }
 
 class HomeState extends State<Home> {
-
   void onPressedAddButton() {
     print("clicked on add button");
   }
 
   @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
+  Widget build(BuildContext context) {
+    return new Scaffold(
         appBar: new AppBar(
           title: new Text("HomeX"),
           actions: <Widget>[
@@ -43,7 +58,6 @@ class HomeState extends State<Home> {
           child: new Center(
             child: new CircularProgressIndicator(),
           ),
-        )
-      );
-    }
+        ));
+  }
 }
